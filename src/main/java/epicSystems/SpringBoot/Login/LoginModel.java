bebf070d54871f1077ef7f9317b91ec80c09ce78
venkatesh Pittala <venkirowling@gmail.com>
2017-07-24 17:03:37 -0400

    " Rest API user details

---
+++ src/main/java/epicSystems/SpringBoot/Login/LoginModel.java
@@ -0,0 +1,40 @@
+package epicSystems.SpringBoot.Login;
+
+public class LoginModel {
+	
+	private String username;
+	
+	private String password;
+	
+	public LoginModel()
+	{
+		
+	}
+	
+	
+	public LoginModel(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+
+}
---
+++ src/main/java/epicSystems/SpringBoot/LoginApi.java
@@ -0,0 +1,15 @@
+package epicSystems.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+SpringApplication.run(LoginApi.class,args);
+	}
+
+}
