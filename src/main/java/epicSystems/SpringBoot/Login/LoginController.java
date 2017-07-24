05330002901cb006dad5860454d79efeed0956f1
venkatesh Pittala <venkirowling@gmail.com>
2017-07-24 19:00:01 -0400

    url mapping updated

--- src/main/java/epicSystems/SpringBoot/Login/LoginController.java
+++ src/main/java/epicSystems/SpringBoot/Login/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class LoginController {
 
-	@RequestMapping("/users")
+	@RequestMapping("/Epicusers")
 	
 	public List<LoginModel> getAllusers() {
 		 
